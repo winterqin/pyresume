5fbddb99140ab097bea5952f15e7b623db5fec0a
QinWinter <qinwinter@qq.com>
2025-09-02 16:52:31 +0800

    move django to backend dir

--- start_backend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo 启动Django后端服务器...
-cd /d "%~dp0"
-uv run python manage.py runserver 8000
-pause 
--- start_frontend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo 启动前端开发服务器...
-cd /d "%~dp0\frontend"
-npm run dev
-pause 
